96132ef6a46cddc8bd3e479a047f7e253772f558
aaron <aaron.vmale@gmail.com>
2021-01-03 11:36:10 -0800

    turned board into 2d array

--- lib/board.rb
+++ lib/board.rb
@@ -1,63 +1,68 @@
 require 'colorize'
 class ChessBoard
     def initialize
-        @board_array = Array.new(64," ")
+        @board_array = Array.new(8," ") {Array.new(8," ")}
 
     end
     attr_accessor :board_array
     def print_board
         @s=1
-        @piece=""
-        @board_array.each_with_index do |space, i|
-            if space !=" "
-                @piece=space.symbol
-            else
-                @piece = " "
+        @square=""
+        @board_array.each_with_index do |sub, i|
+            sub.each_with_index do |row_element,j|
+                if row_element !=" "
+                    @square=row_element.symbol
+                else
+                    @square = " "
+                end
+                if @s.even?
+                    print " #{@square} ".colorize(:background => :light_black)
+                    @s+=1
+                else
+                    print " #{@square} ".colorize(:background => :black)
+                    @s+=1
+                end
             end
-            if (@s)%2==0
-                print " #{@piece} ".colorize(:background => :light_black)
-                @s+=1
-            else
-                print " #{@piece} ".colorize(:background => :black)
-                @s+=1
-            end
-            if (i+1)%8==0 &&i!=0
-                @s+=1
-                puts""
-            end
-            
+            puts""    
+            @s+=1
         end
 
     
     end
-    def set_pieces #sets all pieces to starting position
-        @board_array[48..55].each_with_index do |space,i|
-            @board_array[i+48]=WhitePawn.new(i+48)
+    def set_pieces #sets all squares to starting position
+        @board_array[6].each_with_index do |sub,i|
+            @board_array[6][i]=WhitePawn.new(1)
         end
-        @board_array[7..14].each_with_index do |space,i|
-            @board_array[i+8]=BlackPawn.new(i+8)
-            end
-        @board_array[56]=WhiteRook.new(56)
-        @board_array[63]=WhiteRook.new(63)
-        @board_array[57]=WhiteKnight.new(57)
-        @board_array[62]=WhiteKnight.new(62)
-        @board_array[58]=WhiteBishop.new(58)
-        @board_array[61]=WhiteBishop.new(61)
-        @board_array[59]=WhiteQueen.new(59)
-        @board_array[60]=WhiteKing.new(60)
 
-        @board_array[0]=BlackRook.new(0)
-        @board_array[7]=BlackRook.new(7)
-        @board_array[1]=BlackKnight.new(1)
-        @board_array[6]=BlackKnight.new(6)
-        @board_array[2]=BlackBishop.new(2)
-        @board_array[5]=BlackBishop.new(5)
-        @board_array[3]=BlackQueen.new(3)
-        @board_array[4]=BlackKing.new(4)
+        @board_array[1].each_with_index do |space,i|
+            @board_array[1][i]=BlackPawn.new(1)
+        end
 
-        @board_array[23..46].each_with_index do |space,i|
-            @board_array[i+24]=" "
+        @board_array[7][0]=WhiteRook.new(1)
+        @board_array[7][7]=WhiteRook.new(63)
+        @board_array[7][1]=WhiteKnight.new(57)
+        @board_array[7][6]=WhiteKnight.new(62)
+        @board_array[7][2]=WhiteBishop.new(58)
+        @board_array[7][5]=WhiteBishop.new(61)
+        @board_array[7][3]=WhiteQueen.new(59)
+        @board_array[7][4]=WhiteKing.new(60)
+
+        @board_array[0][0]=BlackRook.new(1)
+        @board_array[0][7]=BlackRook.new(63)
+        @board_array[0][1]=BlackKnight.new(57)
+        @board_array[0][6]=BlackKnight.new(62)
+        @board_array[0][2]=BlackBishop.new(58)
+        @board_array[0][5]=BlackBishop.new(61)
+        @board_array[0][3]=BlackQueen.new(59)
+        @board_array[0][4]=BlackKing.new(60)
+
+
+
+        @board_array[2..5].each_with_index do |sub,i| #erases the empty squares in mid
+            sub.each_with_index do |square,j|
+                @board_array[i+2][j]=" "
             end
+        end
     
          
     end
--- lib/sample.rb
+++ lib/sample.rb
@@ -1,8 +1,8 @@
-@spaces[56]=BlackRook.new
-@spaces[63]=BlackRook.new
-@spaces[57]=BlackKnight.new
-@spaces[62]=BlackKnight.new
-@spaces[58]=BlackBishop.new
-@spaces[61]=BlackBishop.new
-@spaces[59]=BlackQueen.new
-@spaces[60]=BlackKing.new
+@board_array[0][0]=BlackRook.new(1)
+@board_array[0][7]=BlackRook.new(63)
+@board_array[0][1]=BlackKnight.new(57)
+@board_array[0][6]=BlackKnight.new(62)
+@board_array[0][2]=BlackBishop.new(58)
+@board_array[0][5]=BlackBishop.new(61)
+@board_array[0][3]=BlackQueen.new(59)
+@board_array[0][4]=BlackKing.new(60)
